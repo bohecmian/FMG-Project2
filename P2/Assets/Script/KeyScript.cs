f78d83bd657a2ffca597b2bbb102ab6eb9acf935
bohecmian <bohecmian@gmail.com>
2024-09-24 00:18:05 -0400

    KeyScript

---
+++ P2/Assets/Script/KeyScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject doorCollider;
+
+
+    void Start()
+    {
+        doorCollider.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            doorCollider.SetActive(true);
+            Destroy(gameObject);
+    }
+}
